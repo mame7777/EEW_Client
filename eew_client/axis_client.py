643ec038f9a486e0070f13f6f67d37e6ee3e4e23
mame7777 <86618354+mame7777@users.noreply.github.com>
2024-10-10 14:14:13 +0900

    [update] 記法を更新

--- eew_client/axis_client.py
+++ eew_client/axis_client.py
@@ -56,7 +56,9 @@ class AXISClient:
     def on_error(self, ws: websocket.WebSocket, error):
         print(error)
 
-    def on_close(self, ws: websocket.WebSocket, close_status_code, close_msg):
+    def on_close(
+        self, ws: websocket.WebSocket, close_status_code, close_msg
+    ):  # pylint: disable=unused-argument
         print("### closed ###")
 
     def on_open(self, ws: websocket.WebSocket):
@@ -74,7 +76,7 @@ class AXISClient:
         """
         response = requests.get(
             self.eew_server_list_api_url,
-            headers={"Authorization": "Bearer %s" % self.eew_access_token},
+            headers={"Authorization": f"Bearer {self.eew_access_token}"},
             timeout=6.0,
         )
         if response.status_code != 200:
@@ -109,26 +111,22 @@ class AXISClient:
             ConnectionError: サーバーに接続できない
         """
         server_list = self.get_server_list()
-        for server_list_index in range(len(server_list)):
-            server_url = server_list[server_list_index]
+        for server_url in server_list:
             try:
                 self.ws = websocket.WebSocketApp(
-                    "%s/socket" % server_url,
-                    header=["Authorization: Bearer %s" % self.eew_access_token],
+                    f"{server_url}/socket",
+                    header=[f"Authorization: Bearer {self.eew_access_token}"],
                     on_open=self.on_open,
                     on_message=self.on_message,
                     on_error=self.on_error,
                     on_close=self.on_close,
                 )
+                if self.debug:
+                    print(f"[Info] connected to {server_url}")
                 break
-            except:
-                if server_list_index == len(server_list) - 1:
-                    print("[Error] cannot connect to any server!")
-                    raise ConnectionError()
-                else:
-                    continue
-        if self.debug:
-            print(f"[Info] connected to {server_url}")
+            except Exception as e:
+                print(f"[Error] cannot connect to {server_url}\n{e}")
+                continue
 
         try:
             ws_thread = threading.Thread(
@@ -150,5 +148,6 @@ class AXISClient:
         except KeyboardInterrupt:
             self.ws.close()
         # error everything
-        except:
+        except Exception as e:
+            print(f"[Error] {e}")
             self.ws.close()
